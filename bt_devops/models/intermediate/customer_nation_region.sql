5428a69bf58bbbb26e8d98b51a8f4f591a759553
viswanath <viswanath.bobba@gmail.com>
2021-02-10 11:16:46 -0500

    dbt_devops_copied for test purpose

---
+++ bt_devops/models/intermediate/customer_nation_region.sql
@@ -0,0 +1,16 @@
+WITh customer_nation_region AS (
+  SELECT * from snowflake_sample_data.tpch_sf1.customer
+    JOIN snowflake_sample_data.tpch_sf1.nation
+        ON n_nationkey=c_nationkey
+            JOIN snowflake_sample_data.tpch_sf1.region
+                ON n_regionkey=r_regionkey
+)
+
+SELECT
+r_name AS region,
+n_name AS nation,
+n_nationkey AS nation_key,
+c_name AS customer_name,
+c_acctbal AS account_balance,
+c_mktsegment AS market_segment
+    FROM customer_nation_region
---
+++ bt_devops/models/staging/account_balance_by_part_and_supplier.sql
@@ -0,0 +1,21 @@
+WITH customer_nation_region AS (
+  SELECT * 
+    FROM snowflake_sample_data.tpch_sf1.customer
+      JOIN snowflake_sample_data.tpch_sf1.nation
+      ON n_nationkey=c_nationkey
+        JOIN snowflake_sample_data.tpch_sf1.region
+        ON n_regionkey=r_regionkey
+),
+
+rename as (
+
+SELECT
+    r_name AS region,
+    n_name AS nation,
+    n_nationkey AS nation_key,
+    c_name AS customer_name,
+    c_acctbal AS account_balance,
+    c_mktsegment As market_segment
+  FROM customer_nation_region
+)
+select * from rename
